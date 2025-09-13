fa0c9699065714d11174c966a7f9d1ceb20afece
Don Cross <cosinekitty@gmail.com>
2025-09-13 17:09:56 -0400

    Pendulum 2D model: work in progress.

--- src/rk4_integrator.hpp
+++ src/rk4_integrator.hpp
@@ -24,4 +24,49 @@ namespace CosineKitty
             state += (dt/6)*(k1 + 2*(k2+k3) + k4);
         }
     };
+
+    // `StateVector` is handy for use as a `state_t` with Integrator,
+    // but it is NOT REQUIRED. You can use `double` if your model has a 1D state.
+    // But most of the time you can use StateVector to express any compound state as
+    // an ordered list of numbers.
+    // For example, suppose 4 particles in 3D space have position and velocity (two 3D vectors):
+    // dimensions = 4*3*2 = 24
+    // The aggregate system state is a vector in a 24-dimensional space:
+    // [
+    //      rx0, ry0 rz0,  rx1, ry1, rz1,  rx2, ry2, rz2,  rx3, ry3, rz3,
+    //      vx0, vy0 vz0,  vx1, vy1, vz1,  vx2, vy2, vz2,  vx3, vy3, vz3
+    // ]
+    // This is one example, and you don't have to follow it as a convention.
+    // It is up to the caller to understand what the array index values mean,
+    // and it is an arbitrary mapping for each use case.
+    // This code tries to remain as unopinionated as possible in order
+    // to remain flexibly reusable for a variety of physical systems.
+
+    template <unsigned dimensions, typename real_t>
+    struct StateVector
+    {
+        real_t coord[dimensions]{};
+
+        void operator += (const StateVector& other)
+        {
+            for (unsigned i = 0; i < dimensions; ++i)
+                coord[i] += other.coord[i];
+        }
+
+        friend StateVector operator * (real_t scalar, const StateVector& vec)
+        {
+            StateVector product;
+            for (unsigned i = 0; i < dimensions; ++i)
+                product.coord[i] = scalar * vec.coord[i];
+            return product;
+        }
+
+        friend StateVector operator + (const StateVector& a, const StateVector& b)
+        {
+            StateVector sum;
+            for (unsigned i = 0; i < dimensions; ++i)
+                sum.coord[i] = a.coord[i] + b.coord[i];
+            return sum;
+        }
+    };
 }
--- util/unittest.cpp
+++ util/unittest.cpp
@@ -111,6 +111,61 @@ static int Logarithm()
 
 static int Pendulum()
 {
+    // Goal: model a 2D pendulum of fixed length L, with mass m concentrated at a point on the end.
+    // Represent the state in angular terms.
+    // The displacement angle away from the vertical [radians] is called theta.
+    // The derivative of theta with respect to time, i.e. the angular speed, is called omega.
+    // The angular acceleration (the second time derivative) is called alpha.
+    // The kinematic model is based on analysis in the tangential direction only,
+    // because we assume the pendulum has a fixed length L. Because L is constant,
+    // movement in the radial direction is not possible.
+    // Therefore, we can exclude radial variables from the model without missing anything.
+    // In terms of the RK4 integrator, we consider the state to be the current angular
+    // position and angular speed as a pair: [theta, omega].
+    // The derivative of the state gives another pair: [omega, alpha].
+    // Thus the state type needs 2 numbers.
+    static constexpr unsigned ndim = 2;
+    using state_t = CosineKitty::StateVector<ndim, double>;
+    using integ_t = CosineKitty::Integrator<double, state_t>;
+
+    static constexpr double g = 9.8;    // gravitational acceleration [m/s^2]
+    static constexpr double L = 1;      // length of the pendulum [m]
+
+    // Start the pendulum displaced away from the vertical, but at rest (speed = 0).
+    state_t pendulum;
+    const double initialDisplacementDeg = 5;
+    const double initialDisplacementRad = (M_PI/180) * initialDisplacementDeg;
+    pendulum.coord[0] = initialDisplacementRad;
+    pendulum.coord[1] = 0;
+
+    auto deriv = [](double t, const state_t& state) -> state_t
+    {
+        // Calculate the derivative of [theta, omega] to obtain [omega, alpha].
+        state_t slope;
+        slope.coord[0] = state.coord[1];    // copy angular speed (omega) [rad/s]
+        slope.coord[1] = (g/L) * std::sin(state.coord[0]);    // calculate angular acceleration alpha [rad/s^2]
+        return slope;
+    };
+
+    // We test the pendulum model by running the simulation
+    // and measuring the following properties:
+    //
+    // 1. Time between consecutive axis-crossings (both left and right) should be identical.
+    // 2. In general, because this is a frictionless pendulum, kinetic+potential energy must be conserved.
+    // 3. The maximum and minimum amplitude should be identical.
+    // 4. When run backwards in time, the results should be the same. (Pass in dt < 0)
+
+    integ_t integ(deriv);
+
+    const double dt = 0.01;
+    int zeroCrossingCount = 0;
+    const int zeroCrossingLimit = 100;
+    while (zeroCrossingCount < zeroCrossingLimit)
+    {
+        integ.step(0, dt);      // FIXFIXFIX: passing 't' separately was a mistake in the API
+        break;  // FIXFIXFIX
+    }
+
     printf("Pendulum: PASS\n");
     return 0;
 }
